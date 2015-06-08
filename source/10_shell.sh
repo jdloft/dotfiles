93cf29de78efbeb5f0d58cc78170053d0b82ec1f
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-06-08 08:50:12 -0600

    Number source files correctly

--- source/10_shell.sh
+++ source/10_shell.sh
@@ -40,23 +40,23 @@ unset LC_ALL
 
 if [ -x /usr/bin/dircolors ]
 then
-	eval "`dircolors -b`"
+        eval "`dircolors -b`"
 fi
 
 # Colors http://linux.101hacks.com/ps1-examples/prompt-color-using-tput/
 if [[ $- == *i* ]]
 then
-	CLR_NONE=`tput sgr0`
-	CLR_LINE=`tput smul`
-	CLR_BOLD=`tput bold`
-	CLR_BLACK=`tput setaf 0`
-	CLR_RED=`tput setaf 1`
-	CLR_GREEN=`tput setaf 2`
-	CLR_YELLOW=`tput setaf 3`
-	CLR_BLUE=`tput setaf 4`
-	CLR_MAGENTA=`tput setaf 5`
-	CLR_CYAN=`tput setaf 6`
-	CLR_WHITE=`tput setaf 7`
+        CLR_NONE=`tput sgr0`
+        CLR_LINE=`tput smul`
+        CLR_BOLD=`tput bold`
+        CLR_BLACK=`tput setaf 0`
+        CLR_RED=`tput setaf 1`
+        CLR_GREEN=`tput setaf 2`
+        CLR_YELLOW=`tput setaf 3`
+        CLR_BLUE=`tput setaf 4`
+        CLR_MAGENTA=`tput setaf 5`
+        CLR_CYAN=`tput setaf 6`
+        CLR_WHITE=`tput setaf 7`
 fi
 
 #
