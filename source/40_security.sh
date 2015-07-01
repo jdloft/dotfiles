3881c688f45bfa9350870fe2d81d1d179f1f3f0e
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-06-30 18:54:25 -0600

    Bug: 40_security.sh: Fix bash if condition

--- source/40_security.sh
+++ source/40_security.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # setup ssh keys on login
-if [ ! -z $PS1 ]; then
+if [[ ! -z $PS1 ]]; then
   if [[ -e ~/.ssh/id_rsa || -e ~/.ssh/id_dsa ]]; then
     if [ -z "$SSH_AUTH_SOCK" ]; then
       eval `ssh-agent -s`
