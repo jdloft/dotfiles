0e16c92ebfb621eb0167c40e6945db0a73ad6be7
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-06-30 18:41:28 -0600

    40_security.sh: Add check for key file

--- source/40_security.sh
+++ source/40_security.sh
@@ -2,8 +2,10 @@
 
 # setup ssh keys on login
 if [ ! -z $PS1 ]; then
-  if [ -z "$SSH_AUTH_SOCK" ]; then
-    eval `ssh-agent -s`
-    ssh-add
+  if [[ -e ~/.ssh/id_rsa || -e ~/.ssh/id_dsa ]]; then
+    if [ -z "$SSH_AUTH_SOCK" ]; then
+      eval `ssh-agent -s`
+      ssh-add
+    fi
   fi
 fi
