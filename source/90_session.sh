9c98ae328862af5e7167666a8c2e4655e8dcee6b
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-07-25 02:08:12 +0000

    Add Koding VMs to tmux session

--- source/90_session.sh
+++ source/90_session.sh
@@ -3,7 +3,7 @@
 
 if [[ ! -z $PS1 ]]; then
     if [[ -z $TMUX ]]; then
-        if [ "$DOTFILES_HOST" = "hydrogen" ]; then
+        if [ "$DOTFILES_HOST" = "hydrogen" ] || [ "$DOTFILES_HOST" = "jdloft" ]; then
             SESSIONS="main"
         fi
 
