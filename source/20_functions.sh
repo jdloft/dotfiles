5d45be127369f5d7c941be062debceb907c561f4
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-08-06 17:33:25 +0000

    Fancy display for labs

--- source/20_functions.sh
+++ source/20_functions.sh
@@ -24,7 +24,13 @@ function _dotfiles-prompt() {
         clr_host="$CLR_MAGENTA"
     # WMF labs servers
     elif echo $DOTFILES_HOST | grep -q -E '\.wmflabs'; then
-        clr_host="$CLR_RED"
+        labshost=true
+        site=${host##*.}
+        host=${host%.*}
+        cluster=${host##*.}
+        host=${host%.*}
+        project=${host##*.}
+        host=${host%.*}
     fi
 
     # Root is special
@@ -40,7 +46,11 @@ function _dotfiles-prompt() {
 
     # Actual prompt code
     if [[ -n $supportcolor ]]; then
-        PS1="\[$CLR_WHITE\][\$(date +%H:%M\ %Z)] \[$clr_user\]\u\[$CLR_WHITE\] at \[$clr_host\]$host\[$CLR_WHITE\] in \[$CLR_YELLOW\]\w\$(_dotfiles-git-prompt)\[$CLR_NONE\]\n$(_dotfiles-exit_code $ec)\[$CLR_WHITE\]$prompt\[$CLR_NONE\] "
+        if [ "$labshost" = true ]; then
+            PS1="\[$CLR_WHITE\][\$(date +%H:%M\ %Z)] \[$clr_user\]\u\[$CLR_WHITE\] at \[$CLR_RED\]$host.\[$CLR_BOLD\]$project\[$CLR_NONE\]\[$CLR_RED\].\[$CLR_LINE\]$cluster\[$CLR_NONE\]\[$CLR_RED\].$site\[$CLR_WHITE\] in \[$CLR_YELLOW\]\w\$(_dotfiles-git-prompt)\[$CLR_NONE\]\n$(_dotfiles-exit_code $ec)\[$CLR_WHITE\]$prompt\[$CLR_NONE\] "
+        else
+            PS1="\[$CLR_WHITE\][\$(date +%H:%M\ %Z)] \[$clr_user\]\u\[$CLR_WHITE\] at \[$clr_host\]$host\[$CLR_WHITE\] in \[$CLR_YELLOW\]\w\$(_dotfiles-git-prompt)\[$CLR_NONE\]\n$(_dotfiles-exit_code $ec)\[$CLR_WHITE\]$prompt\[$CLR_NONE\] "
+        fi
     else
         PS1="[\$(date +%H:%M\ %Z)] \u@$host:\w$prompt "
     fi
