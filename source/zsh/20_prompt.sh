09d3352cffd11b60ab62c6ddba5ecfba0d580eb2
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-10-27 21:17:47 -0400

    zsh: Move git and virtualenv prompt info into RPROMPT

--- source/zsh/20_prompt.sh
+++ source/zsh/20_prompt.sh
@@ -39,9 +39,9 @@ function _dotfiles-prompt() {
 
     # Actual prompt code
     if [ "$labshost" = true ]; then
-        echo "%f[$(date +%H:%M\ %Z)] $clr_user%n%f at %F{red}$host.%B$project%b%F{red}.%U$cluster%u%F{red}.$site%f${debian_chroot:+"%F{yellow} ($debian_chroot)%f"} in %F{yellow}${PWD/#$HOME/~}$(_dotfiles-git-prompt)$(_dotfiles-virtualenv-prompt)%f\n$(_dotfiles-exit_code)$promptchar%f "
+        echo "%f[$(date +%H:%M\ %Z)] $clr_user%n%f at %F{red}$host.%B$project%b%F{red}.%U$cluster%u%F{red}.$site%f${debian_chroot:+"%F{yellow} ($debian_chroot)%f"} in %F{yellow}${PWD/#$HOME/~}%f\n$(_dotfiles-exit_code)$promptchar%f "
     else
-        echo "%f[$(date +%H:%M\ %Z)] $clr_user%n%f at $clr_host$host%f${debian_chroot:+"%F{yellow} ($debian_chroot)%f"} in %F{yellow}${PWD/#$HOME/~}$(_dotfiles-git-prompt)$(_dotfiles-virtualenv-prompt)%f\n$(_dotfiles-exit_code)$promptchar%f "
+        echo "%f[$(date +%H:%M\ %Z)] $clr_user%n%f at $clr_host$host%f${debian_chroot:+"%F{yellow} ($debian_chroot)%f"} in %F{yellow}${PWD/#$HOME/~}%f\n$(_dotfiles-exit_code)$promptchar%f "
     fi
 }
 
@@ -97,3 +97,4 @@ function _dotfiles-virtualenv-prompt() {
 }
 
 PROMPT='$(_dotfiles-prompt)'
+RPROMPT='$(_dotfiles-git-prompt)$(_dotfiles-virtualenv-prompt)'
