3595daa37c9d789e2b9bde8097d9b0e2c52347fb
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-10-27 20:47:04 -0400

    zsh: Configure history

--- source/zsh/00_shell.sh
+++ source/zsh/00_shell.sh
@@ -1,13 +1,22 @@
 #!/usr/bin/zsh
 
+# Prompt
 autoload -U promptinit compinit # autoload prompt and completion
 promptinit
 
+# Completion
 compinit
 zstyle ':completion:*' menu select # fancy tab completion menu
 setopt completealiases
 
+setopt autocd
+
+# History
+export HISTSIZE=1000
+export HISTFILE="$HOME/.history"
+export SAVEHIST=$HISTSIZE
 setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_SPACE
 
 setopt PROMPT_SUBST
 
@@ -16,6 +25,17 @@ path=(~/bin $path[@])
 [[ -n "${key[PageUp]}" ]] && bindkey "${key[PageUp]}" history-beginning-search-backward
 [[ -n "${key[PageDown]}" ]] && bindkey "${key[PageDown]}" history-beginning-search-forward
 
-# Colors!
+#
+# Colors
+#
+
 autoload -U colors
 colors
+
+#
+# Misc
+#
+
+export EDITOR=vim
+
+export GREP_OPTIONS='--color=auto'
