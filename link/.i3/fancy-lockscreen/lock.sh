8a2193291f2101534df2875105ffad9722577727
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-11-19 17:41:44 -0700

    Use DPMS for i3 fancy lockscreen

--- link/.i3/fancy-lockscreen/lock.sh
+++ link/.i3/fancy-lockscreen/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-i3lock --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c -i ~/.i3/fancy-lockscreen/lockscreen.png 2> /dev/null
+i3lock -d --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c -i ~/.i3/fancy-lockscreen/lockscreen.png 2> /dev/null
 if [ $? -ne 0 ]; then # failed because of unsupported colors
-    i3lock -c 000000 -i ~/.i3/fancy-lockscreen/lockscreen.png
+    i3lock -d -c 000000 -i ~/.i3/fancy-lockscreen/lockscreen.png
 fi
