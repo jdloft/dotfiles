bb14283f0c8f032f8ff6e3df210a6f7c19e75b4e
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2015-12-18 02:07:42 +0000

    desk: Add bot24 test alias

--- link/.desk/desks/neon/bot24.sh
+++ link/.desk/desks/neon/bot24.sh
@@ -6,5 +6,6 @@ source env/bin/activate
 
 # Install requirements
 alias req="pip install -r requirements.txt"
+alias test="flake8 . --ignore='E501' --exclude=env/,user-config.py,password-config.py"
 
 echo "Testing workspace."
--- link/.desk/desks/tools/bot24.sh
+++ link/.desk/desks/tools/bot24.sh
@@ -6,6 +6,7 @@ source env/bin/activate
 
 # Install requirements
 alias req="pip install -r requirements.txt"
+alias test="flake8 . --ignore='E501' --exclude=env/,user-config.py,password-config.py"
 
 echo "########################################"
 echo "THIS IS THE PRODUCTION MACHINE!"
