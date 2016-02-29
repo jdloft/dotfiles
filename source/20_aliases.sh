3de0fa6ad453c2a903573ca006bd9f73d92ec47f
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2016-02-29 11:05:59 -0500

    Fix shorthand cd aliases

--- source/20_aliases.sh
+++ source/20_aliases.sh
@@ -11,12 +11,11 @@ alias df='df -h'
 
 alias ..='cd ..'
 alias ....='cd ../../'
-alias ......='cd ../../../'
+alias .2='cd ../../'
 alias .3='cd ../../../'
 alias .4='cd ../../../../'
 alias .5='cd ../../../../../'
 alias .6='cd ../../../../../../'
-alias -- -='cd -'
 
 alias mkdir='mkdir -p'
 
