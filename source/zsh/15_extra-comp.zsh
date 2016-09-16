01818ca7767f2aef63af174c84d0302d37e4db91
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2016-09-15 23:19:59 -0600

    Fix ZSH extra comp fpath

--- source/zsh/15_extra-comp.zsh
+++ source/zsh/15_extra-comp.zsh
@@ -1,4 +1,4 @@
 # If extra zsh-completions repo is available, use it
-if [[ -d "$DOTFILES/lib/zsh-completions" ]]; then
-    fpath=("$DOTFILES/lib/zsh-completions" $fpath)
+if [[ -d "$DOTFILES/lib/zsh-completions/src" ]]; then
+    fpath=("$DOTFILES/lib/zsh-completions/src" $fpath)
 fi
