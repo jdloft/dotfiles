058696dfcf570d8a67d4933bfd9cfa4a76eeab00
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2016-11-07 07:07:09 -0700

    Remove old init files

--- init/repair-ssh.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -h ~/.ssh ]; then
-    echo "~/.ssh is a symlink, repairing"
-    rm ~/.ssh
-    mkdir ~/.ssh
-    mv ~/.dotfiles/link/.ssh/* ~/.ssh
-    rm -r ~/.dotfiles/link/.ssh
-    echo "Repair complete!"
-fi
