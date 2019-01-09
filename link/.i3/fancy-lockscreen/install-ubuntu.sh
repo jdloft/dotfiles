192333a0de6ce54275baf90fc97aec40217e5079
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2019-01-09 13:38:40 -0700

    Remove i3 fance lock

--- link/.i3/fancy-lockscreen/install-ubuntu.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd ~/.i3/fancy-lockscreen/i3lock-color
-sudo apt-get install pkg-config libx11-dev libpam0g-dev libcairo2-dev libxcb1-dev libxcb-dpms0-dev libxcb-image0-dev libxcb-util0-dev libev-dev libxcb-xinerama0-dev libxkbcommon-dev libxkbfile-dev libx11-xcb-dev libxcb-keysyms1-dev
-make
-sudo make install
--- link/.i3/fancy-lockscreen/lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-i3lock -d --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c -p default -i ~/.i3/fancy-lockscreen/lockscreen.png 2> /dev/null
-if [ $? -ne 0 ]; then # failed because of unsupported colors
-    i3lock -d -c 000000 -p default -i ~/.i3/fancy-lockscreen/lockscreen.png
-fi
