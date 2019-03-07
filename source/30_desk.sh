df35c7c4df81e3c29c77104821077c36a691633d
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2019-03-06 19:55:07 -0700

    desk: Don't sync desks

--- source/30_desk.sh
+++ source/30_desk.sh
@@ -2,14 +2,3 @@
 
 # Hook for desk activation
 [ -n "$DESK_ENV" ] && source "$DESK_ENV" || true
-
-# Set desk dir based on host
-if [ "$DOTFILES_HOST" = "jdloft" ]; then
-    export DESK_DESKS_DIR=~/.desk/desks/neon/
-elif [[ $DOTFILES_LHOST == *".tools.eqiad.wmflabs" ]]; then
-    export DESK_DESKS_DIR=~/.desk/desks/tools/
-elif [ "$DOTFILES_HOST" = "titanium" ]; then
-    export DESK_DESKS_DIR=~/.desk/desks/titanium/
-elif [ "$DOTFILES_HOST" = "hydrogen" ]; then
-    export DESK_DESKS_DIR=~/.desk/desks/hydrogen/
-fi
