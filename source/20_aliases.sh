a6ddf25dd97e4d381297851275091bea7ac7e49c
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2020-08-21 10:46:44 -0600

    Fix mac detect and python

--- source/20_aliases.sh
+++ source/20_aliases.sh
@@ -1,5 +1,5 @@
 # On Mac (Darwin), this should be `ls -G` if coreutils isn't installed from homebrew
-if is_mac && [[ `which ls` == '/bin/ls' ]]; then
+if is_mac; then
     alias ls='ls -GF' # -G is colorized output, -F is to show trailing slashes and other file info
 else
     alias ls='ls --color=auto'
