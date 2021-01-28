5aa53418acf2a34c7ea2df65e3aff157f2efc06f
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2021-01-27 20:00:09 -0700

    Add llvm and gcloud sdk to macos path

--- source/50_path.sh
+++ source/50_path.sh
@@ -1,5 +1,30 @@
 # Setup PATH
 
+# macOS specific stuff
+
+# Python
+# TODO: add Linux support
+if is_mac; then
+    if [ -d "$HOME/Library/Python" ]; then
+        for directory in ~/Library/Python/*/; do
+            if [ -d "${directory}bin" ]; then # output from for has a trailing slash
+                path_append "${directory}bin"
+            fi
+        done
+    fi
+
+    # LLVM is keg-only
+    if [ -d "/usr/local/opt/llvm/bin" ]; then
+        path_prepend "/usr/local/opt/llvm/bin"
+    fi
+
+    # Google SDK
+    if [ -d "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk" ]; then
+        source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
+        path_append /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/bin
+    fi
+fi
+
 # Add dotfiles bin dir
 path_prepend "$DOTFILES/bin"
 
@@ -16,12 +41,3 @@ if [ -d "$HOME/.npm/bin" ]; then
     path_prepend "$HOME/.npm/bin"
 fi
 
-# Python
-# TODO: add Linux support
-if is_mac && [ -d "$HOME/Library/Python" ]; then
-    for directory in ~/Library/Python/*/; do
-        if [ -d "${directory}bin" ]; then # output from for has a trailing slash
-            path_append "${directory}bin"
-        fi
-    done
-fi
