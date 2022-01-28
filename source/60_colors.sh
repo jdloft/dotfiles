cdccac8f219b1802cd4d7f0a493660ecf72611b2
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2022-01-28 14:19:18 -0700

    Only run colors if interactive

--- source/60_colors.sh
+++ source/60_colors.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-if [ $SOLAR_MODE3 = true ]; then
-    eval $DOTFILES/scripts/solar_mode3.sh
-fi
+if [[ $- == *i* ]]; then
+    if [ $SOLAR_MODE3 = true ]; then
+        eval $DOTFILES/scripts/solar_mode3.sh
+    fi
 
-# Solarized dircolors
-if ! is_mac; then
-    eval `dircolors $DOTFILES/resources/dircolors-solarized/dircolors.ansi-dark`
+    # Solarized dircolors
+    if ! is_mac; then
+        eval `dircolors $DOTFILES/resources/dircolors-solarized/dircolors.ansi-dark`
+    fi
 fi
