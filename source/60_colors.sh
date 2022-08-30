e52bdf8e92b6cbffd8fe082dbb67f01f5c4aff1c
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2022-08-30 14:34:13 -0700

    Don't exec solar_mode3.sh

--- source/60_colors.sh
+++ source/60_colors.sh
@@ -2,6 +2,6 @@
 
 if [[ $- == *i* ]]; then # if we're interactive
     if [ $SOLAR_MODE3 = true ]; then
-        eval $DOTFILES/scripts/solar_mode3.sh
+        eval sh $DOTFILES/scripts/solar_mode3.sh
     fi
 fi
