d7ae31d17eec8cd558bc5ffda7596068bc31aaed
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2022-08-30 14:49:31 -0700

    Add back dircolors-solarized

--- source/60_colors.sh
+++ source/60_colors.sh
@@ -4,4 +4,9 @@ if [[ $- == *i* ]]; then # if we're interactive
     if [ $SOLAR_MODE3 = true ]; then
         eval sh $DOTFILES/scripts/solar_mode3.sh
     fi
+
+    # Solarized dircolors
+    if type dircolors > /dev/null 2>&1; then
+        eval `dircolors $DOTFILES/resources/dircolors-solarized/dircolors.ansi-dark`
+    fi
 fi
