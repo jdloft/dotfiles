4479e08b0c544a40891d4d7cdafcfb57267e372f
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2022-10-20 16:06:26 -0700

    Work around conda zsh issue

--- source/zsh/60_completions.zsh
+++ source/zsh/60_completions.zsh
@@ -6,12 +6,23 @@ if [ -f "/opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/complet
     source /opt/homebrew/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 fi
 
+patch_conda() {
+    # workaround for https://github.com/spf13/cobra/issues/1694
+    # kubectl and helm
+    # https://github.com/kubernetes/kubernetes/issues/105587
+    if [[ $1 == "kubectl" || $1 == "helm" ]]; then
+        sed -i 's/tab=$(printf \x27\\t\x27)/tab="$(printf \x27\\t\x27)"/' $ZSH_CACHE_DIR/completions/_$1
+    fi
+}
+
 cache_completion() {
     if (( $+commands[$1] )); then
         if [[ ! -f "$ZSH_CACHE_DIR/completions/_$1" ]]; then
             $1 completion zsh | tee "$ZSH_CACHE_DIR/completions/_$1" >/dev/null
+            patch_conda $1
             source "$ZSH_CACHE_DIR/completions/_$1"
         else
+            patch_conda $1
             source "$ZSH_CACHE_DIR/completions/_$1"
             $1 completion zsh | tee "$ZSH_CACHE_DIR/completions/_$1" >/dev/null &|
         fi
