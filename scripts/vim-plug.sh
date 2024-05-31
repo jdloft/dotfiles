94357e6704ebca913c244bbcf162ab5bbcf0bd86
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2024-05-31 19:25:43 -0400

    Vim plug config doesn't exist

--- scripts/vim-plug.sh
+++ scripts/vim-plug.sh
@@ -3,4 +3,4 @@ if ! command -v vim &> /dev/null; then
     echo -e "\033[0;31mVim is not installed!\033[0m"
     exit 1
 fi
-vim -u ~/.vim/vim-plugged.vim +PlugClean +PlugInstall +PlugUpdate +qall
+vim +PlugClean +PlugInstall +PlugUpdate +qall
