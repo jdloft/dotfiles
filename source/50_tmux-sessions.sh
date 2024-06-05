0a222517f22355666c083869a083487630e008cd
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2024-06-04 21:02:29 -0600

    fixup! Add tmux session reminder

--- source/50_tmux-sessions.sh
+++ source/50_tmux-sessions.sh
@@ -1,7 +1,7 @@
 # Alert if Tmux sessions are open
 if command -v "tmux" > /dev/null 2>&1; then
     num_sessions=`tmux list-sessions 2> /dev/null | wc -l`
-    if [ "$?" -eq 0 ]; then
+    if [[ "$?" -eq 0 && "$num_sessions" -gt 0 ]]; then
         echo "There are $((num_sessions)) Tmux sessions opened"
     fi
 fi
