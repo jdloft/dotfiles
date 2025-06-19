a6ef2f143622e793fe262174ef3d1927bf10316d
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2025-06-18 18:57:22 -0600

    Short-circuit single tmux session output

--- source/50_tmux.sh
+++ source/50_tmux.sh
@@ -9,7 +9,7 @@ if [[ $- == *i* ]] && command -v "tmux" > /dev/null 2>&1; then
     num_sessions=`tmux list-sessions 2> /dev/null | wc -l`
     if [ "$?" -eq 0 ] && [ "$num_sessions" -gt 0 ] && [ ! "$TMUX" ]; then
         if [ "$num_sessions" -eq 1 ]; then
-            echo "There is $((num_sessions)) Tmux session opened"
+            echo "There is 1 Tmux session opened"
         else
             echo "There are $((num_sessions)) Tmux sessions opened"
         fi
