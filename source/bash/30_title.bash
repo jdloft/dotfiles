e9729f01e6ed84319b444e83384aa858374c370b
Jamison Lofthouse <younger-mosaic.7n@icloud.com>
2026-03-31 19:28:44 +0000

    Title stuff

--- source/bash/30_title.bash
+++ source/bash/30_title.bash
@@ -1,10 +1,9 @@
 if [[ "$TERM" != "dumb" && "$TERM" != "linux" ]]; then
-    autoload -U add-zsh-hook
     function _dotfiles_titleinfo() {
         local host="$DOTFILES_HOST"
         local rel="${PWD#$HOME}"
         local display="$([[ "$rel" != "$PWD" ]] && echo "~$rel" || echo "$PWD")"
         printf '\033]0;%s@%s:%s\033\\' "$USER" "$host" "$display"
     }
-    add-zsh-hook precmd _dotfiles-titleinfo
+    PROMPT_COMMAND="_dotfiles_titleinfo${PROMPT_COMMAND:+;$PROMPT_COMMAND}"
 fi
