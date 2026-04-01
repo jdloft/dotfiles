2ee985f1a183bd2aa9857c11fe2cdeaa209fa0cc
Jamison Lofthouse <younger-mosaic.7n@icloud.com>
2026-03-31 21:04:55 -0400

    fix: titleinfo zsh

--- source/zsh/30_title.zsh
+++ source/zsh/30_title.zsh
@@ -6,5 +6,5 @@ if [[ "$TERM" != "dumb" && "$TERM" != "linux" ]]; then
         local display="$([[ "$rel" != "$PWD" ]] && echo "~$rel" || echo "$PWD")"
         printf '\033]0;%s@%s:%s\033\\' "$USER" "$host" "$display"
     }
-    add-zsh-hook precmd _dotfiles-titleinfo
+    add-zsh-hook precmd _dotfiles_titleinfo
 fi
